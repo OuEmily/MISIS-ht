7045ad86330abf65b12e0870ceae2437e5896e8e
OuEmily <114305635+OuEmily@users.noreply.github.com>
2023-04-28 13:27:01 +0300

    Add files via upload

--- yuldasheva_e_d/prj.lab/arrayd/include/arrayd/arrayd.hpp
+++ yuldasheva_e_d/prj.lab/arrayd/include/arrayd/arrayd.hpp
@@ -1,23 +1,28 @@
+#pragma once
+
+#ifndef ARRAYDHPP
+#define ARRAYDHPP
+
 #include <cstddef>
 #include <iostream>
 
 
 
-class Arrayd {
+class ArrayD {
 public:
-	Arrayd() = default;
+	ArrayD() = default;
 	
 	// {
 	// 	start_ = new double[capacity_]; {0.0;}
 	// }
-	Arrayd(const std::ptrdiff_t n); // контруктор создает массив заданного размера n
-	Arrayd(const Arrayd& rhs); // конструктор копирования
+	ArrayD(const std::ptrdiff_t n); // контруктор создает массив заданного размера n
+	ArrayD(const ArrayD& rhs); // конструктор копирования
 	//Arrayd(const std::ptrdiff_t& size, const doubl& fill);
 
 	double& operator[](std::ptrdiff_t i); // для присваивания
 	const double& operator[](const std::ptrdiff_t i) const; // для чтения
-	Arrayd& operator=(const Arrayd& arr);
-	//arrayd& operator=(const arrayd& arr) = default;
+	ArrayD& operator=(const ArrayD& arr);
+	//ArrayD& operator=(const ArrayD& arr) = default;
 
 
 	void resize(const std::ptrdiff_t new_size);
@@ -26,14 +31,14 @@ public:
 	void remove(const std::ptrdiff_t ind); // удаление элемента с текущим индексом
 	std::ptrdiff_t ssize() const noexcept;
 // const после функции говорит что в функции ничего не изменяется
-	~Arrayd() = default;
+	~ArrayD() = default;
 
 private:
 	std::ptrdiff_t array_size_ = { 0 };//фактический размер
 	std::ptrdiff_t capacity_ = { 0 };// логический размер (незаполненная часть массива)
 	double* start_ = { nullptr }; // отсылает на невалидный адресс (непосредственно наш массив)
 };
-/* arrayd(std::initializer_list<double> rhs) {
+/* ArrayD(std::initializer_list<double> rhs) {
 		double* new_start = new double[rhs.size()];
 		if (new_start != nullptr) {
 			for (int i = 0; i < rhs.size(); i += 1) {
@@ -44,3 +49,5 @@ private:
 			start_ = new_start;
 		}
 	} */
+
+#endif 
